ed83cf5cd77aca3da1b84e399ec0958b16c6a7ae
Jaap <jaap@tuxic.nl>
2018-05-19 11:06:34 +0200

    more led pins

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include "LPC17xx.h"
 #include <stdint.h>
 
+// led pins on MBED lpc1768:
+#define LED4_PIN (18)
+#define LED4_PIN (20)
+#define LED4_PIN (21)
 #define LED4_PIN (23)
 
 int main() {
@@ -11,7 +15,7 @@ int main() {
   SystemInit();
   
     // Setup P1.23 as output
-  LPC_GPIO1->FIODIR |= (1 << LED4_PIN);
+  LPC_GPIO1->FIODIR |= (1 << LED1_PIN);
     
   for(;;) {
     for(uint32_t delay = 0; delay < 10000000; delay++) {
@@ -19,13 +23,13 @@ int main() {
     }
     
     // Turn LED ON
-    LPC_GPIO1->FIOSET = (1 << LED4_PIN);
+    LPC_GPIO1->FIOSET = (1 << LED1_PIN);
     
     for(uint32_t delay = 0; delay < 10000000; delay++) {
        __asm("NOP");
     }
     
     // Turn LED OFF
-    LPC_GPIO1->FIOCLR = (1 << LED4_PIN);
+    LPC_GPIO1->FIOCLR = (1 << LED1_PIN);
   }
 }
